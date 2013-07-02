4718be8ceb6ded6bfc318c63ba96fd1929d4d049
Tim Visher <tvisher@monetate.com>
2013-07-02 18:48:58 -0400

    Updating project.clj with license and home page, etc. EOM

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject what-does-tools-trace-do "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "A very basic tutorial for a rather simple library: tools.trace"
+  :url "https://github.com/timvisher/what-does-tools-trace-do"
+  :license {:name "Attribution CC BY"
+            :url "http://creativecommons.org/licenses/by/3.0/legalcode"}
   :dependencies [[org.clojure/clojure     "1.5.1"]
                  [org.clojure/tools.trace "0.7.5"]])
